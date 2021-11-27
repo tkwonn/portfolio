df702a7d629d93f7a7b7c9c4cc043eecd2792081
Taesok Kwon <kwontaesok1997@gmail.com>
2021-11-27 04:09:45 -0800

    add AppContext Provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,17 @@
+import React, {useState} from 'react';
 import Theme from '../styles/theme';
 import {AppProps} from 'next/app';
 
+import AppContext from '../context/AppContext';
+
 const App = ({ Component, pageProps }: AppProps) => {
+  const [ language, setLanguage ] = useState("English");
   return (
-    <>
+    <AppContext.Provider value={{language, setLanguage}}>
       <Theme>
         <Component {...pageProps} />
       </Theme>
-    </>
+    </AppContext.Provider>
   );
 }
 
