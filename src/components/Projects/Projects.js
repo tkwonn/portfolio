ed0a730d3407660fa302a0480ac74a884d2b94e4
Taesok Kwon <kwontaesok1997@gmail.com>
2022-01-09 19:22:38 -0800

    add cli-app

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -13,7 +13,7 @@ const Projects = () => (
     <SectionTitle main>Projects</SectionTitle>
     <SectionText style={{ fontSize: "1.5rem", color: "#f3905bd5"}}>** Team Projects are highlighted **</SectionText>
     <GridContainer>
-      {teamProjects.map(({ id, image, title, description, points, tags, hashtags, visit, source }) => (
+      {teamProjects.map(({ id, image, title, description, tags, hashtags, visit, source }) => (
         <BlogCard style={{ border: "2px solid #f49867", boxShadow: "3px 3px 10px #f3905bd5" }} key={id.toString()}>
           <Img src={image} />
           <TitleContent>
@@ -38,7 +38,7 @@ const Projects = () => (
           </UtilityList>
         </BlogCard>
       ))}
-      {projects.map(({ id, image, title, description, points, tags, visit, source }) => (
+      {projects.map(({ id, image, title, description, hashtags, tags, visit, source }) => (
         <BlogCard key={id.toString()}>
           <Img src={image} />
           <TitleContent>
@@ -49,6 +49,11 @@ const Projects = () => (
             <TagList>
               {tags.map((tag, index) => (
                 <Tag key={index.toString()}><AiOutlineTags /> {tag}</Tag>
+              ))}
+            </TagList>
+            <TagList>
+              {hashtags.map((ele, index) => (
+                <HashTag key={index.toString()}># {ele}</HashTag>
               ))}
             </TagList>
           </div>
