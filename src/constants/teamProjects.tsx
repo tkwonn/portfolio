9962bc212decf573ae3e6c3eb80ef4a6d9aa2484
Taesok Kwon <kwontaesok1997@gmail.com>
2022-02-06 11:43:54 -0800

    add etch-a-sketch project to projectList

--- src/constants/teamProjects.tsx
+++ src/constants/teamProjects.tsx
@@ -1,23 +1,25 @@
 export const teamProjects = [
   {
+    title: 'Etch A Sketch',
+    description: "In Etch a Sketch, you can view your own saved drawings at gallery page as well as all published drawings by other users! Draw using Classic Etch A Sketch Mode and adjust line thickness and line color to make your creations totally unique! You can also upload your own drawings to Twitter.",
+    points: "",
+    image: '/images/etch-a-sketch.png',
+    tags: ['Django', 'Vue', 'Konva.js'],
+    hashtags: ['REST API', 'VueX'],
+    source: 'https://github.com/Recursion-Group-K',
+    visit: 'https://sketch-skgl.herokuapp.com/signup/',
+    id: 1,
+  },
+  {
     title: 'Connect 4',
     description: "Connect 4 is a turn-based game where two players take turns dropping colored balls from the top into a column. The ball will fall straight down towards the next available space within the column. The aim of each player is to be the first to form a vertical, horizontal, or diagonal line of four of one's own balls. The purpose of this project is mainly to implement the algorithm and test its performance in a simple game.",
     points: "",
-    image: '/images/connect4-2.png',
-    tags: ['Next.js', 'Material UI'],
+    image: '/images/connect4.png',
+    tags: ['React', 'Material UI'],
     hashtags: ['OOP', 'Monte Carlo Tree Search'],
     source: 'https://github.com/TeamE-React/connect-4',
     visit: 'https://teame-connect4.vercel.app/',
     id: 1,
   },
-  // {
-  //   title: 'Connect 4',
-  //   description: "This project is a four in row game (player vs computer) that has been written in Vue.js. This was also my first opportunity to develop in a team and to use Github with multiple people.　We designed it to dynamically generate the board size and the number of players, and used Vuex as a state management method. For a win algorithm, we were able to reduce the number of computations by treating the last checker as a focal point.",
-  //   points: "",
-  //   image: '/images/connect4.jpg',
-  //   tags: ['Vue'],
-  //   source: 'https://github.com/Recursion-TeamE/Connect4',
-  //   visit: 'https://recursion-teame.github.io/Connect4/',
-  //   id: 1,
-  // },
+  
 ]
