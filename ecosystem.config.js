e76bed359544c3aba5f3219b61666515f15e66b0
Taesok Kwon <kwontaesok1997@gmail.com>
2024-05-19 01:26:47 -0700

    pm2 setup

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps : [{
+    script: 'npm start',
+  }],
+
+  deploy : {
+    production : {
+      user : 'ubuntu',
+      host : '52.36.19.230',
+      ref  : 'origin/main',
+      repo : 'https://github.com/tkwonn/portfolio.git',
+      path : '/home/ubuntu',
+      'pre-deploy-local': '',
+      'post-deploy' : 'source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': '',
+      'ssh_options': 'ForwardAgent=yes',
+    }
+  }  
+};
