eaa4fce6f9e35ea70f105ba90f418f0baa279e35
Taesok Kwon <kwontaesok1997@gmail.com>
2025-02-03 22:17:31 -0800

    Add imgdock project

--- src/lib/webProjects.ts
+++ src/lib/webProjects.ts
@@ -1,6 +1,7 @@
 import { Project } from '@/type';
 import clickEmpire from '../../public/assets/img/click-empire.png';
 import connect4 from '../../public/assets/img/connect4.png';
+import imgdock from '../../public/assets/img/imgdock.png';
 import plantuml from '../../public/assets/img/plantuml.png';
 import portfolio from '../../public/assets/img/portfolio.png';
 import sketch from '../../public/assets/img/sketch.png';
@@ -9,10 +10,27 @@ import video from '../../public/assets/img/video.png';
 
 export const projects = [
     {
+        image: imgdock,
+        title: 'Online Image Hosting Service',
+        description:
+            'An Imgur-like application that enables anonymous image sharing without user accounts. Key features include an image uploading with tags, unique URL generation, category-based browsing, multiple view modes (grid/waterfall), etc.',
+        tags: [
+            'TypeScript',
+            'PHP',
+            'memcached',
+            'Docker',
+            'Amazon EC2',
+            'Amazon RDS',
+            'Amazon S3',
+        ],
+        githubUrl: 'https://github.com/tkwonn/imgdock',
+        appUrl: 'https://imgdock.com/',
+    },
+    {
         image: snippet,
         title: 'Text Snippet Sharing Service',
         description:
-            'A Pastebin-like web application that enables instant sharing of text/code snippets. Features include syntax highlighting for multiple programming languages, customizable expiration times, and public/unlisted sharing options.',
+            'A Pastebin-like application that enables instant sharing of text and code snippets. Key features include a syntax highlighting for multiple programming languages, customizable expiration times, and public/unlisted sharing options.',
         tags: ['JavaScript', 'PHP', 'MySQL', 'Amazon EC2', 'Amazon RDS'],
         githubUrl: 'https://github.com/tkwonn/text-snippet',
         appUrl: 'https://text-snippet.taesokkwon.com/',
@@ -21,34 +39,34 @@ export const projects = [
         image: plantuml,
         title: 'PlantUML Server',
         description:
-            'A web tool for software engineers to practice creating UML diagrams using PlantUML syntax. Key features include a problem collection for UML practice, interactive UML diagram creation, and options to download diagrams. Cheat sheets with example UML diagrams and source code are also available as guidance.',
+            'A web tool for practicing and learning UML diagram creation with PlantUML syntax. Key features include a problem collection for UML practice, interactive UML diagram creation, and options to download diagrams. Cheat sheets with example UML diagrams and source code are also provided.',
         tags: ['JavaScript', 'PHP', 'Amazon EC2'],
         githubUrl: 'https://github.com/tkwonn/plantuml',
         appUrl: 'https://plantuml.taesokkwon.com/',
     },
     {
-        image: video,
-        title: 'Video Compressor',
-        description:
-            'A desktop application that lets users compress and convert videos with the ffmpeg library. Built with Electron, it facilitates efficient event handling by linking the main process, which accesses the OS via Node.js, with the renderer process, which manages the HTML DOM.',
-        tags: ['Electron', 'TypeScript', 'React', 'Tailwind CSS'],
-        githubUrl: 'https://github.com/tkwonn/video-compressor',
-        appUrl: 'https://github.com/tkwonn/video-compressor/releases',
-    },
-    {
         image: portfolio,
         title: 'Portfolio Website',
         description:
-            "Building my portfolio website gave me hands-on experience with domain setup, configuring a web server, deploying on a cloud services, and setting up CI/CD pipelines. This site is fully static by using Next.js's Static Export and does not require Node.js.",
+            "A personal portfolio website built with Next.js Static Export for static content delivery. Deployed on AWS EC2 with Nginx reverse proxy, automated deployments via GitHub Actions, and HTTPS encryption using Let's Encrypt.",
         tags: ['TypeScript', 'Next.js', 'Tailwind CSS', 'Amazon EC2'],
         githubUrl: 'https://github.com/tkwonn/portfolio',
         appUrl: '/',
     },
     {
+        image: video,
+        title: 'Video Compressor',
+        description:
+            'A desktop application that allows users compress and convert videos with the ffmpeg library. Built with Electron, it facilitates efficient event handling by linking the main process, which accesses the OS via Node.js, with the renderer process, which manages the HTML DOM.',
+        tags: ['Electron', 'TypeScript', 'React', 'Tailwind CSS'],
+        githubUrl: 'https://github.com/tkwonn/video-compressor',
+        appUrl: 'https://github.com/tkwonn/video-compressor/releases',
+    },
+    {
         image: sketch,
         title: 'Etch A Sketch',
         description:
-            'Etch A Sketch is a web-based version of the classic Etch A Sketch drawing toy. Using Konva.js for canvas functionality, it allows users to adjust line color and thickness and share their creations with friends. The application includes a secure Web API built with the Django REST Framework, using JWT.',
+            'A drawing app inspired by the classic Etch A Sketch toy. Uses Konva.js for canvas drawing and supports line style customization. User sketches are stored in Cloudinary while user profiles and drawing metadata are persisted in PostgreSQL. Backend uses Django REST API with JWT authentication.',
         tags: ['Vue.js', 'Django', 'PostgreSQL'],
         githubUrl: 'https://github.com/Recursion-Group-K/sketch',
         appUrl: 'https://sketch-frontend-d11d98fe289b.herokuapp.com/',
@@ -57,7 +75,7 @@ export const projects = [
         image: connect4,
         title: 'Connect 4',
         description:
-            "Connect 4 is a turn-based game where two players take turns dropping colored balls into a grid, competing to align four balls vertically, horizontally, or diagonally. I implemented an AI opponent feature using the Monte Carlo Tree Search algorithm and managed the application state using React's Context API.",
+            'A turn-based game where two players compete to align four balls vertically, horizontally, or diagonally. Implements an AI opponent using Monte Carlo Tree Search algorithm and manages state with React Context API.',
         tags: ['HTML5', 'CSS3', 'React'],
         githubUrl: 'https://github.com/TeamE-React/connect-4',
         appUrl: 'https://teame-connect4.vercel.app/',
@@ -66,7 +84,7 @@ export const projects = [
         image: clickEmpire,
         title: 'Clicker Empire Game',
         description:
-            'For my first individual project, I built a browser application inspired by Cookie Clicker, utilizing JavaScript, the DOM API, and Local Storage. This project helped me deepen my understanding of browser rendering, create interactive user experiences, and learn to store data directly in the browser.',
+            'Cookie Clicker-inspired browser game using JavaScript and DOM manipulation. Key features include an interactive gameplay and local storage for saving progress.',
         tags: ['HTML5', 'CSS3', 'JavaScript'],
         githubUrl: 'https://github.com/tkwonn/clicker-empire-game',
         appUrl: 'https://clicker-empire-game-five.vercel.app/',
