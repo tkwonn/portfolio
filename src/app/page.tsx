0beae7b6574bd40c3a8572676bd22fe1d1b487c3
Taesok Kwon <kwontaesok1997@gmail.com>
2025-02-17 20:57:14 -0800

    wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
-import { redirect } from 'next/navigation';
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
 export default function RootPage() {
-    redirect('/en');
+    const router = useRouter();
+    useEffect(() => {
+        router.replace('/en');
+    }, [router]);
 }
